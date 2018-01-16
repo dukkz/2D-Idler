bb21f5c7d386718805b8275c6f0884da39bbb2b8
dukkz <dukkz24@gmail.com>
2018-01-16 14:05:56 -0600

    Initial commit

---
+++ Assets/Scripts/AreaDraw.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaDraw : MonoBehaviour {
+
+	public float xSize, ySize;
+
+	void OnDrawGizmos(){
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube(transform.position, new Vector3(xSize, ySize));
+	}
+}
