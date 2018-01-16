e8534e3eed3e04aa8dc10eb0ddc1c8d8390f75e7
dukkz <dukkz24@gmail.com>
2018-01-16 15:03:00 -0600

    Enemy Movement
    Made simple player following script. Needs work.

---
+++ Assets/Scripts/EnemyMotor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMotor : MonoBehaviour {
+
+	public Transform player;
+	public float distanceToTarget, speed = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distanceToTarget = (player.position - transform.position).magnitude;
+
+		if (distanceToTarget > 3) {
+			transform.position = Vector3.MoveTowards (transform.position, player.position, speed);
+		} else {
+			transform.position = transform.position;
+		}
+
+	}
+}
